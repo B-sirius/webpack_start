6407f85b89e844511a5a83800f9c2b2528a23272
b-sirius <padfoot_07@163.com>
2017-10-07 10:57:43 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import _ from 'lodash';
+import printMe from './print.js';
+import './style.css';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Fuck off', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
     return element;
 }
 
 document.body.appendChild(component());
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log('Accepting the updated printMe module');
+        printMe();
+    })
+}
